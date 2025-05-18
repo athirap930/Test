fabbc7bf81e9a688818136d9192fdd4e1cc93a91
athirap930 <athirap930@gmail.com>
2025-05-18 23:46:44 +0530

    Setup basic React application

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Rajdhani Minerals</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-gray-800">Rajdhani Minerals</h1>
+      </div>
     </div>
   )
 }
